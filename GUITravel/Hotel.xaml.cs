0bb29c20104da2b50e21422864a498168d56b9d9
frithjofhoppe <frithjof.hoppe@icloud.com>
2018-02-22 09:30:51 +0100

    firstCommut

---
+++ GUITravel/Hotel.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUITravel
+{
+    /// <summary>
+    /// Interaction logic for Hotel.xaml
+    /// </summary>
+    public partial class Hotel : UserControl
+    {
+        public Hotel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ GUITravel/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUITravel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Travel";
+        }
+
+        private void btnHotel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Title = "Hotel";
+            this.showPlace.Children.Clear();
+            this.showPlace.Children.Add(new Hotel());
+        }
+
+        private void btnKunde_Click(object sender, RoutedEventArgs e)
+        {
+            this.Title = "Kunde";
+            this.showPlace.Children.Clear();
+            this.showPlace.Children.Add(new Kunde());
+        }
+    }
+}
